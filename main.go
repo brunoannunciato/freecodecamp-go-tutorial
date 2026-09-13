bd78349db5b4886d06474bcfd8e6c0be6df8793c
brunoannunciato <brunobannunciato@gmail.com>
2026-09-13 00:59:36 -0300

    :sparkles: create connection with mongodb atlas

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type Todo struct {
@@ -12,6 +16,8 @@ type Todo struct {
 	Body      string `json: "body"`
 	Completed bool   `json: "completed"`
 }
+
+var collection *mongo.Collection
 
 func main() {
 	fmt.Println("App running")
@@ -21,4 +27,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected with MongoDB Atlas")
 }
